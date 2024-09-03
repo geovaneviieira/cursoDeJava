bd4c90439ba5118f45a44f666ad9f08d5befd0b5
geovaneviieira <you@example.com>
2024-09-03 20:49:26 -0300

    mudanca

--- Meu-primeiro-projeto-java/src/App.java
+++ Meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Boas vindas turma DIO");
+        Scanner metodoScanner = new Scanner(System.in);
+
+        String metodoNome = metodoScanner.nextLine();
+        System.out.println(metodoNome);
+
+    
     }
 }
